c224e6691eb49b10eaa34c679e56f6b69845ab7b
DIEGO <di_fsantos@yahoo.com.br>
2018-10-02 23:22:01 -0300

    New feature add

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,35 +12,36 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee */
 public class MainActivity extends AppCompatActivity {
 
-    private int quantityCoffee = 0;
-    private int priceOneCoffee = 5;
+    private int quantityCoffee = 2;
+    private int priceOneCoffee = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayInitial(quantityCoffee, priceOneCoffee);
     }
 
     /*
      * This method is called when the order button is clicked*/
     public void submitOrder(View view) {
-        quantityCoffee++;
         display(quantityCoffee);
         displayPrice(quantityCoffee * priceOneCoffee);
     }
 
     /*
-    * This method is called when to arrive the moment of the show at information for the user */
-    private void display(int number) {
+     * This method is called when to arrive the moment of the show at information for the user */
+    private void display(int quantity) {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(number));
+        quantityTextView.setText(String.valueOf(quantity));
     }
 
     /*
-    * This method is called when to arrive the moment of the show the value of the coffee for user */
-    private void displayPrice(int number) {
+     * This method is called when to arrive the moment of the show the value of the coffee for user */
+    private void displayPrice(int price) {
         TextView valuePriceTextView = findViewById(R.id.value_price_text_view);
-        valuePriceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        valuePriceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
     }
 
     /* Method that is used for clear the fields in the program */
@@ -52,9 +53,33 @@ public class MainActivity extends AppCompatActivity {
             quantityTextView.setText(String.valueOf(quantityCoffee));
 
             TextView valuePrice = findViewById(R.id.value_price_text_view);
-            valuePrice.setText(String.valueOf("R$" + quantityCoffee));
+            valuePrice.setText(NumberFormat.getCurrencyInstance().format(quantityCoffee));
 
             Toast.makeText(this, "Limpeza realizada com sucesso", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    /*
+     * This method is called when the plus button is clicked */
+    public void increment(View view) {
+        // int quantity = 3;
+        // display(quantity);
+        display(++quantityCoffee);
+    }
+
+    /*
+     * This method is called when the minus button is clicked */
+    public void decrement(View view) {
+        //int quantity = 1;
+        //display(quantity);
+        if (quantityCoffee > 0) {
+            quantityCoffee--;
+        }
+        display(quantityCoffee);
+    }
+
+    private void displayInitial(int quantityCoffeeInitial, int priceCoffeeInitial) {
+        display(quantityCoffeeInitial);
+        displayPrice(priceCoffeeInitial);
     }
 }
